5ba0f25433c1521d60425b928c11055271f0bf69
anto777molina777@gmail.com <anto777molina777@gmail.com>
2022-08-04 10:18:32 -0500

    tabla con direcion clas

--- vegetta777/models.py
+++ vegetta777/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 class djmariio(models.Model):
-    nombre = models.CharField(max_length=20)
-    canalYOUTUBE = models.CharField(max_length=20)
-    descripcion = models.TextField(blank=True)
+    nombre        = models.CharField(max_length=20)
+    canalYOUTUBE  = models.CharField(max_length=20)
+    descripcion   = models.TextField(blank=True)
+   
+class clientes(models.Model):
+    nombre    = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email     = models.EmailField()
+    telefonoo      = models.CharField(max_length=7)
+    
+class Articulus(models.Model):
+    nombre  = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=20)
+    precio  = models.IntegerField()
+    
+class Pedidos(models.Model):
+    numero     = models.IntegerField()
+    fecha      = models.DateField()
+    entregado  = models.BooleanField()
